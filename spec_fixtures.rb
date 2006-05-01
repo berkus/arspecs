301367fb9d547e5fee2d64911250eda537fc2550
berkus <berkus@motoko.madfire.net>
2006-05-01 23:15:50 +0000

    Added transactions, cleaned code a little bit.

--- spec_fixtures.rb
+++ spec_fixtures.rb
@@ -4,51 +4,68 @@ require 'active_record/fixtures'
 class FixturesHandler
   cattr_accessor :fixture_path
   cattr_accessor :fixture_table_names
+  cattr_accessor :use_transactional_fixtures
   
-  @@fixture_path = RAILS_ROOT + "/test/fixtures/"
-
-  def initialize(table_names = nil)
-    @@fixture_table_names = table_names.to_a unless table_names.nil?  ###FIXME?
+  self.fixture_path = RAILS_ROOT + "/test/fixtures/"
+  self.fixture_table_names = []
+  self.use_transactional_fixtures = false
+	    
+  def initialize(table_names)
+    fixtures(table_names)
   end
 
-  def self.fixtures=(*table_names)
+  def self.fixtures(*table_names)
     table_names = table_names.flatten.map { |n| n.to_s }
     self.fixture_table_names |= table_names
     require_fixture_classes(table_names)
   end
 					      
-def self.require_fixture_classes(table_names)
-  table_names.each do |table_name| 
-    file_name = table_name.to_s
-    file_name = file_name.singularize if ActiveRecord::Base.pluralize_table_names
-    begin
-      require file_name
-    rescue LoadError
-      # Let's hope the developer has included it himself
-      puts $!
+  def self.require_fixture_classes(table_names)
+    table_names.each do |table_name| 
+      file_name = table_name.to_s
+      file_name = file_name.singularize if ActiveRecord::Base.pluralize_table_names
+      begin
+        require file_name
+      rescue LoadError
+        # Let's hope the developer has included it himself
+      end
     end
   end
-end
 
-def load_fixtures
-  @loaded_fixtures = {}
-  fixtures = Fixtures.create_fixtures(@@fixture_path, @@fixture_table_names)
-  unless fixtures.nil?
-    if fixtures.instance_of?(Fixtures)
-      @loaded_fixtures[fixtures.table_name] = fixtures
-    else
-      fixtures.each { |f| @loaded_fixtures[f.table_name] = f }
+  def use_transactional_fixtures?
+    self.use_transactional_fixtures
+  end
+
+  def load_fixtures
+    @loaded_fixtures = {}
+    fixtures = Fixtures.create_fixtures(@@fixture_path, @@fixture_table_names)
+    unless fixtures.nil?
+      if fixtures.instance_of?(Fixtures)
+        @loaded_fixtures[fixtures.table_name] = fixtures
+      else
+        fixtures.each { |f| @loaded_fixtures[f.table_name] = f }
+      end
+    end
+  
+    if use_transactional_fixtures?
+      ActiveRecord::Base.lock_mutex
+      ActiveRecord::Base.connection.begin_db_transaction
     end
   end
-end
 
-def teardown_fixtures
-end
+  def teardown_fixtures
+    # Rollback changes.
+    if use_transactional_fixtures?
+      ActiveRecord::Base.connection.rollback_db_transaction
+      ActiveRecord::Base.unlock_mutex
+    end
+    ActiveRecord::Base.clear_connection_cache!
+  end
 end
 
 module Kernel
   def fixtures(*table_names)
-    FixturesHandler.fixtures = table_names
+    FixturesHandler.fixtures(table_names)
   end
 
   def context(name, *table_names, &block)
