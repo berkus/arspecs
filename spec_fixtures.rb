c3e5f20bcfc779f7bf5a6f32d926a5e178c30300
berkus <berkus@motoko.madfire.net>
2006-05-01 23:27:48 +0000

    Added normal transactions.

--- spec_fixtures.rb
+++ spec_fixtures.rb
@@ -5,36 +5,56 @@ class FixturesHandler
   cattr_accessor :fixture_path
   cattr_accessor :fixture_table_names
   cattr_accessor :use_transactional_fixtures
-  
-  self.fixture_path = RAILS_ROOT + "/test/fixtures/"
-  self.fixture_table_names = []
-  self.use_transactional_fixtures = false
-	    
+
+  @@fixture_path = RAILS_ROOT + "/test/fixtures/"
+  @@fixture_table_names = []
+  @@use_transactional_fixtures = false
+  @@already_loaded_fixtures = {}
+      	    
   def initialize(table_names)
     fixtures(table_names)
   end
 
   def self.fixtures(*table_names)
     table_names = table_names.flatten.map { |n| n.to_s }
-    self.fixture_table_names |= table_names
+    @@fixture_table_names |= table_names
     require_fixture_classes(table_names)
-  end
-					      
-  def self.require_fixture_classes(table_names)
-    table_names.each do |table_name| 
-      file_name = table_name.to_s
-      file_name = file_name.singularize if ActiveRecord::Base.pluralize_table_names
-      begin
-        require file_name
-      rescue LoadError
-        # Let's hope the developer has included it himself
-      end
-    end
   end
 
   def use_transactional_fixtures?
     self.use_transactional_fixtures
   end
+
+  def setup
+    # Load fixtures once and begin transaction.
+    if use_transactional_fixtures?
+      if @@already_loaded_fixtures[self.class]
+        @loaded_fixtures = @@already_loaded_fixtures[self.class]
+      else
+        load_fixtures
+        @@already_loaded_fixtures[self.class] = @loaded_fixtures
+      end
+
+      ActiveRecord::Base.lock_mutex
+      ActiveRecord::Base.connection.begin_db_transaction
+
+    # Load fixtures for every test.
+    else
+      @@already_loaded_fixtures[self.class] = nil
+      load_fixtures
+    end
+  end
+
+  def teardown
+    # Rollback changes.
+    if use_transactional_fixtures?
+      ActiveRecord::Base.connection.rollback_db_transaction
+      ActiveRecord::Base.unlock_mutex
+    end
+    ActiveRecord::Base.clear_connection_cache!
+  end
+
+  private
 
   def load_fixtures
     @loaded_fixtures = {}
@@ -46,20 +66,18 @@ class FixturesHandler
         fixtures.each { |f| @loaded_fixtures[f.table_name] = f }
       end
     end
-  
-    if use_transactional_fixtures?
-      ActiveRecord::Base.lock_mutex
-      ActiveRecord::Base.connection.begin_db_transaction
-    end
   end
 
-  def teardown_fixtures
-    # Rollback changes.
-    if use_transactional_fixtures?
-      ActiveRecord::Base.connection.rollback_db_transaction
-      ActiveRecord::Base.unlock_mutex
+  def self.require_fixture_classes(table_names)
+    table_names.each do |table_name| 
+      file_name = table_name.to_s
+      file_name = file_name.singularize if ActiveRecord::Base.pluralize_table_names
+      begin
+        require file_name
+      rescue LoadError
+        # Let's hope the developer has included it himself
+      end
     end
-    ActiveRecord::Base.clear_connection_cache!
   end
 end
 
@@ -70,8 +88,8 @@ module Kernel
 
   def context(name, *table_names, &block)
     @fixtures = FixturesHandler.new(table_names)
-    @fixtures.load_fixtures
+    @fixtures.setup
     Spec::Runner::Context.new(name, &block)
-    @fixtures.teardown_fixtures
+    @fixtures.teardown
   end
 end
