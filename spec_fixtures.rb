90a6f8e1277d88830fa18c7c59133656cfa1d1d7
berkus <berkus@motoko.madfire.net>
2006-05-02 02:51:25 +0000

    Better looking version. Still problems loading fixtures tho (?).

--- spec_fixtures.rb
+++ spec_fixtures.rb
@@ -1,7 +1,7 @@
 require 'spec'
 require 'active_record/fixtures'
 
-class FixturesHandler
+class FixturesHandler < Spec::Runner::Context
   cattr_accessor :fixture_path
   cattr_accessor :fixture_table_names
   cattr_accessor :use_transactional_fixtures
@@ -11,21 +11,37 @@ class FixturesHandler
   @@use_transactional_fixtures = false
   @@already_loaded_fixtures = {}
       	    
-  def initialize(table_names)
-    fixtures(table_names)
+  def initialize(*args)
+    puts "initialize"
+    super
   end
 
   def self.fixtures(*table_names)
+    puts "fixtures(#{table_names.inspect})"
     table_names = table_names.flatten.map { |n| n.to_s }
     @@fixture_table_names |= table_names
     require_fixture_classes(table_names)
   end
 
   def use_transactional_fixtures?
+    puts "use_transactional_fixtures?"
     self.use_transactional_fixtures
   end
 
-  def setup
+  def setup(&block)
+    local_setup = method :setup_fixtures
+    super { local_setup.call; block.call }
+  end
+  
+  def teardown(&block)
+    local_teardown = method :teardown_fixtures
+    super { block.call; local_teardown.call }
+  end
+
+  private
+
+  def setup_fixtures
+    puts "setup"
     # Load fixtures once and begin transaction.
     if use_transactional_fixtures?
       if @@already_loaded_fixtures[self.class]
@@ -37,26 +53,30 @@ class FixturesHandler
 
       ActiveRecord::Base.lock_mutex
       ActiveRecord::Base.connection.begin_db_transaction
+      puts "transaction begin"
 
     # Load fixtures for every test.
     else
       @@already_loaded_fixtures[self.class] = nil
       load_fixtures
     end
+    
+    raise "No fixtures loaded!" if @loaded_fixtures.empty?
   end
 
-  def teardown
+  def teardown_fixtures
+    puts "teardown"
     # Rollback changes.
     if use_transactional_fixtures?
       ActiveRecord::Base.connection.rollback_db_transaction
       ActiveRecord::Base.unlock_mutex
+      puts "transaction end"
     end
     ActiveRecord::Base.clear_connection_cache!
   end
 
-  private
-
   def load_fixtures
+    puts "load_fixtures"
     @loaded_fixtures = {}
     fixtures = Fixtures.create_fixtures(@@fixture_path, @@fixture_table_names)
     unless fixtures.nil?
@@ -69,6 +89,7 @@ class FixturesHandler
   end
 
   def self.require_fixture_classes(table_names)
+    puts "require_fixture_classes(#{table_names.inspect})"
     table_names.each do |table_name| 
       file_name = table_name.to_s
       file_name = file_name.singularize if ActiveRecord::Base.pluralize_table_names
@@ -86,10 +107,9 @@ module Kernel
     FixturesHandler.fixtures(table_names)
   end
 
-  def context(name, *table_names, &block)
-    @fixtures = FixturesHandler.new(table_names)
-    @fixtures.setup
-    Spec::Runner::Context.new(name, &block)
-    @fixtures.teardown
+  def fixture_context(name, *table_names, &block)
+    puts "context(#{name}, #{table_names.inspect})"
+    FixturesHandler.fixtures(table_names) if table_names.size > 0
+    FixturesHandler.new(name, &block)
   end
 end
--- specs/something_spec.rb
+++ specs/something_spec.rb
@@ -1,24 +1,32 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
-#fixtures :somethings
+puts "Start"
 
-context "A first object" do
+fixtures :somethings
+
+fixture_context "A first object" do
   setup do
+    puts "setup1"
     @some = Something.find_by_id(1)
   end
   
   specify "is really a first object" do
+    puts "specify1"
     @some.text.should.equal "First"
   end
 end
 
-context "A second object", :somethings do
+fixture_context "A second object", :somethings do
   setup do
+    puts "setup2"
     @second = Something.find_by_id(2)
   end
   
   specify "is really a second object" do
+    puts "specify2"
     @second.text.should.not.equal "First"
     @second.text.should.equal "Another"
   end
 end
+
+puts "End"
